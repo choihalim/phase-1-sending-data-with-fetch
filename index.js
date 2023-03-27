2246b35ebc4d849d47ac1ae9fc7ecec21b0d41ee
Halim Choi <hchoicareer@gmail.com>
2023-03-27 14:50:35 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your code here
+function submitData(name, email) {
+    let url = "http://localhost:3000/users"
+    let formData = {
+        name: name,
+        email: email
+    }
+    return fetch(url,{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(formData)
+    })
+    .then(response => response.json())
+    .then(data => document.body.append(data.id))
+    .catch(error => {
+        document.body.append(error.message);
+    });
+}
